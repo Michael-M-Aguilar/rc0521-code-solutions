4d8f38c4424c815ad61895f8eca3c1af5eb87693
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-05-10 11:32:01 -0700

    Completed javascript-operators-and-expressions.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 5;
+var height = 10;
+var area = width * height;
+console.log('value of area:' + area);
+console.log('typeof area:' + typeof area);
+
+var bill = 43.50;
+var payment = 50.00;
+var change = bill - payment;
+console.log('value of change:' + change);
+console.log('typeof change:' + typeof change);
+
+var quizzes = 72;
+var midterm = 84;
+var final = 96;
+var grades = (quizzes + midterm + final) / 3;
+console.log('value of grades:' + grades);
+console.log('typeof grades:' + typeof grades);
+
+var firstName = 'Michael';
+var lastName = 'Aguilar';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:' + fullName);
+console.log('typeof fullName:' + typeof fullName);
+
+var pH = 6;
+var isAcidic = (pH < 7);
+console.log('value of isAcidic:' + isAcidic);
+console.log('typeof isAcidic' + typeof isAcidic);
+
+var headCount = 86;
+var isSparta = (headCount < 300);
+console.log('value of isSparta:' + isSparta);
+console.log('typeof isSparta:' + typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('value of motto:' + motto);
+console.log('typeof motto:' + typeof motto);

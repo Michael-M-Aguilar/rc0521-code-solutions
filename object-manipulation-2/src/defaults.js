ad0e5c7cfc96c229995240fee6960fa5f599b1ce
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-05-18 10:15:27 -0700

    Last Problem

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,18 @@
 /* exported defaults */
+function defaults(target, source) {
+  var object = {};
+  var targetValues = Object.keys(target);
+  var sourceValues = Object.keys(source);
+  for (var i = 0; i < targetValues.length; i++) {
+    for (var j = 0; j < sourceValues.length; i++) {
+      if (targetValues[i] !== sourceValues[i]) {
+        object = object[sourceValues[i]];
+      }
+    }
+  }
+  // for(var k in object {
+  //   if (object[k] === undefined) {
+  //     delete object[k]
+  //   }
+  // }
+}

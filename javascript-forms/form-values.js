e5240addec1e22c6d9f57a0ee51078b3f5ec5893
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-05-19 14:42:42 -0700

    Checkpoint.

---
+++ javascript-forms/form-values.js
@@ -0,0 +1,7 @@
+// var $form = document.querySelector('#contact-form');
+
+// $form.addEventListener('submit', preventDefault(event){
+//   event.preventDefault();
+
+//   document.getElementById('contact-form').reset();
+// })

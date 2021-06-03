ee1c26d98c0a19f1052c6b659683486f3ec8d352
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-06-03 13:34:17 -0700

    Checkpoint Lunch.

---
+++ javascript-carousel/main.js
@@ -0,0 +1,39 @@
+// var $rotation = document.querySelector('.rotation');
+// var $allCircles = document.getElementsByClassName('fa-circle');
+// // Have an array of objects to access later as a carousel?
+// var images = [
+//   {
+//     src: 'images/001.png',
+//     alt: 'Bulbasaur'
+//   },
+//   {
+//     src: 'images/004.png',
+//     alt: 'Charmander'
+//   },
+//   {
+//     src: 'images/007.png',
+//     alt: 'Squirtle'
+//   },
+//   {
+//     src: 'images/025.png',
+//     alt: 'Pikachu'
+//   },
+//   {
+//     src: 'images/039.png',
+//     alt: 'Jigglypuff'
+//   }
+// ];
+// // set a counter, where if it reaches a number, a certain image will show up?
+// var counter = 0
+// function scrollRight {
+//   counter++
+//   // once it hits the last image, reset
+//   if(counter === 4) {
+//     counter = 0
+//   }
+//   //When the counter reaches a certain number, it will set the new image class equal to that src/alt values.
+//   $rotation.setAttribute('src', images[counter].src)
+//   $rotation.setAttribute('alt', images[counter].alt)
+//   // how to change the circles to be the dark filled in ones?
+
+// }

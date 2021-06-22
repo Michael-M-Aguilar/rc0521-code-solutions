e566f6e9a7700e23dc3c84b07e007bd8694eb473
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-06-22 16:32:13 -0700

    Completed node-require.

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -1,18 +1,7 @@
-// const add = require('./add');
-// const subtract = require('./subtract');
-// const multiply = require('./multiply');
-// const divide = require('./divide');
-
 const add = require('./add');
 const divide = require('./divide');
 const multiply = require('./multiply');
 const subtract = require('./subtract');
-
-// process.argv = add(a, b) => {
-//   console.log(a + b);
-// };
-// console.log(add);
-console.log(process.argv[2]);
 
 const first = Number(process.argv[2]);
 const second = Number(process.argv[4]);

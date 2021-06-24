c18256488159c6390c75d55ca78b7940593159d1
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-06-24 12:59:53 -0700

    Completed update function!

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -2,10 +2,13 @@ const fs = require('fs');
 const add = require('./add');
 const data = require('./data.json');
 const read = require('./read');
+const update = require('./update');
 const filler = process.argv[2];
 
 if (filler === 'read') {
   read(data, fs);
 } else if (filler === 'create') {
   add(data, fs);
+} else if (filler === 'update') {
+  update(data, fs);
 }
--- node-notes-cli/update.js
+++ node-notes-cli/update.js
@@ -1,4 +1,12 @@
-// const update = (data, fs) => {
-//   const notePicker = process.argv[3];
-//   const newNote = process.argv[4];
-// };
+const update = (data, fs) => {
+  const index = process.argv[3];
+  const updatedNote = process.argv[4];
+  data.notes[index] = updatedNote;
+
+  const jsonStringify = JSON.stringify(data, null, 2);
+  fs.writeFile('data.json', jsonStringify, 'utf8', err => {
+    if (err) throw err;
+  });
+};
+
+module.exports = update;

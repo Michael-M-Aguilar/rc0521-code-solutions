d84a26db2fea34d820c1786b2c556d5405d2491e
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-06-29 00:02:37 -0700

    Completed express-post-json.

---
+++ express-post-json/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+let nextId = 1;
+const grades = {};
+
+app.get('/api/grades', (req, res) => {
+  const array = [];
+  for (const key in grades) {
+    array.push(grades[key]);
+  }
+  res.json(array);
+});
+
+const JSON = express.json();
+app.use(JSON);
+
+app.post('/api/grades', (req, res) => {
+  grades[nextId] = req.body;
+  grades[nextId].id = nextId;
+  const curId = nextId;
+  nextId++;
+  res.status(201).json(grades[curId]);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Sever is running and listening at port 3000!');
+});

94b40715ec509a03d14dc83f03fdb0a65924c78a
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-07-09 12:34:17 -0700

    Completed react-hot-button-component.

---
+++ react-hot-button-component/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ButtonChange from './hot-button';
+
+ReactDOM.render(
+  <ButtonChange/>,
+  document.querySelector('#root')
+);

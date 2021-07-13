32859bc1b89696dc5c5982d209e07d77e3e32144
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-07-13 15:56:45 -0700

    Checkpoint.  Messing around with Map method now.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,91 +1,78 @@
 import React from 'react';
 
+// const tabs = [
+//   { header: 'html', name: 'Hypertext Markup Language', content: 'Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications. With Cascading Style Sheets(CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.' },
+//   { header: 'css', name: 'Cascading Stylesheets', content: 'Cascading Styl Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornsertsone technology of the World Wide Web alongside HTML and JavaScript.' },
+//   { header: 'js', name: 'JavaScript', content: 'JavaScript, often abbreviated as JS, is a high-level interpreted programming language that conforms to the ECMAScript specification.JavaScript has curly- bracket syntax, dynamic typing, prototype - based object - orientation and first - class functions.' }
+// ];
+
 export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { htmlClick: false, cssClick: false, jsClick: false };
-    this.htmlSwitch = this.htmlSwitch.bind(this);
-    this.cssSwitch = this.cssSwitch.bind(this);
-    this.jsSwitch = this.jsSwitch.bind(this);
-    this.htmlClass = this.htmlClass.bind(this);
-    this.cssClass = this.cssClass.bind(this);
-    this.jsClass = this.jsClass.bind(this);
+    this.state = { itemId: false };
+    this.tabSwitch = this.tabSwitch.bind(this);
+    this.mapping = this.mapping.bind(this);
   }
 
-  htmlSwitch(event) {
-    if (event.target.className === 'htmlTab') {
-      this.setState(prevState => ({
-        htmlClick: !prevState.htmlClick
-      }));
+  tabSwitch(event) {
+    if (event.target.className === 'htmlTab' || event.target.className === 'htmlText') {
+      this.setState({
+        itemId: true
+      });
+    // } else if (event.target.className === 'cssTab' || event.target.className === 'cssText') {
+    //   this.setState(prevState => ({
+    //     cssClick: !prevState.cssClick
+    //   }));
+    // } else if (event.target.className === 'jsTab' || event.target.className === 'jsText') {
+    //   this.setState(prevState => ({
+    //     jsClick: !prevState.jsClick
+    //   }));
+    // }
     }
   }
 
-  cssSwitch(event) {
-    if (event.target.className === 'cssTab') {
-      this.setState(prevState => ({
-        cssClick: !prevState.cssClick
-      }));
-    }
-  }
+  mapping() {
+    const tabs = this.props.tabs;
+    const tabsRender = tabs.map(tab =>
+      <div key={ tab.header }>
+        <h3 className={ tab.header + 'Tab'} onClick={this.tabSwitch}>
+          {tab.name}
+        </h3>
+        <p className={this.state.itemId}>
+          {tab.content}
+        </p>
+      </div>
+    );
 
-  jsSwitch(event) {
-    if (event.target.className === 'jsTab') {
-      this.setState(prevState => ({
-        jsClick: !prevState.jsClick
-      }));
-    }
-  }
-
-  htmlClass() {
-    if (this.state.htmlClick === false) {
-      return 'htmlOff';
-    } else {
-      return 'htmlOn';
-    }
-  }
-
-  cssClass() {
-    if (this.state.cssClick === false) {
-      return 'cssOff';
-    } else {
-      return 'cssOn';
-    }
-  }
-
-  jsClass() {
-    if (this.state.jsClick === false) {
-      return 'jsOff';
-    } else {
-      return 'jsOn';
-    }
+    return tabsRender;
   }
 
   render() {
     return (
-  <div>
-    <div className="htmlTab" onClick={this.htmlSwitch}>
-      <h3>Hypertext Markup Language</h3>
-        <div className={this.htmlClass()}>
-          <p>Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications.
-          With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web. </p>
+      <div>
+        {this.mapping()}
       </div>
-    </div>
-    <div className="cssTab" onClick={this.cssSwitch}>
-      <h3>Cascading Style Sheets</h3>
-      <div className={this.cssClass()}>
-        <p>Cascading Styl Sheets (CSS) is a style sheet language used for describing the presentation of a document
-        written in a markup language like HTML. CSS is a cornsertsone technology of the World Wide Web alongside HTML and JavaScript.</p>
-      </div>
-    </div>
-    <div className="jsTab" onClick={this.jsSwitch}>
-      <h3>JavaScript</h3>
-      <div className={this.jsClass()}>
-        <p>JavaScript, often abbreviated as JS, is a high-level interpreted programming language that conforms to
-        the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based
-        object-orientation and first-class functions.</p>
-      </div>
-    </div>
-  </div>
     );
   }
 }
+
+// <div className="accordion">
+//   <div className="htmlTab" onClick={this.tabSwitch}>
+//     <h3 className="htmlText">Hypertext Markup Language</h3>
+//     <div className="">
+//       ...
+//     </div>
+//   </div>
+//   <div className="cssTab" onClick={this.tabSwitch}>
+//     <h3 className="cssText">Cascading Style Sheets</h3>
+//     <div className="">
+//       ...
+//     </div>
+//   </div>
+//   <div className="jsTab" onClick={this.tabSwitch}>
+//     <h3 className="jsText">JavaScript</h3>
+//     <div className="">
+//       ...
+//     </div>
+//   </div>
+// </div>
